5ec759f16146e47ae8a086fb7cc34e2d0a5464bc
Boba Fett <gitfett@luv2spd.com>
2013-02-08 14:06:23 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1123e5b39df2b1d2dbba26e723e3cb7e84e5841b0c90ab8abfede1b792924c6b8db103c462b07909b24570cc97f81b60f231a8447137204bb15fef447f77ae65'
